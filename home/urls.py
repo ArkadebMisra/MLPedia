a2a2c067ec4ea2050853d081fd4825db739d5e21
Sayan <bhattacharya.sayan30@gmail.com>
2022-04-02 11:46:28 +0530

    git ignore issused

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    #urls for Neural Net
-    path("",views.home, name = "home"),
-]
--- plotter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("draw_line_graph", views.draw_line_graph, name="draw_line_graph"),
-]
