d32d29ac3357d02056507b3dfce52bd8af41399b
ArkadebMisra <misra.arkadeb@gmail.com>
2024-07-25 18:43:36 +0530

    pythonanywhere

--- MLPedia/settings.py
+++ MLPedia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-aw97-ojf2!pwff29^%7fu&-r4eh=#rltgdgfjew%iw50hmn==x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ArkadebMisra.pythonanywhere.com'] #in local host just []
 
 
 # Application definition
@@ -130,8 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+#in  local host next line
 STATIC_URL = 'static/'
 
+#in production python anywhere
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# ----------------------------------
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
